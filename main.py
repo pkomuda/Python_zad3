6349da82e60cb475f08222a72bce223c109ba780
Przemysław Komuda <p.komuda1@gmail.com>
2019-11-27 12:56:49 +0100

    Średnia liczona here

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import pandas as pd
+
 if __name__ == '__main__':
-    print("main")
+    data = pd.read_csv("iris.data", names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+    iris = pd.DataFrame(data)
+    print(iris)
+    print('Sepal length mean: ' + str(iris['sepal_length'].mean()))
